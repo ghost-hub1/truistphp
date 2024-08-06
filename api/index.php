719daadf6d502cfaa5b30a3229a9a6de9745dddf
ghost-hub1 <37017254+ghost-hub1@users.noreply.github.com>
2024-08-06 19:47:44 +0100

    fucking change

--- api/index.php
+++ api/index.php
@@ -1,4 +1,4 @@
 <?php
-header('Location: /../login.html');
+header('Location: /../index.php');
 exit
 ?>
--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
-header('Location: /../public/login.html');
+header('Location:/public/login.html');
 exit
 ?>
